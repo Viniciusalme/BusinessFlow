73433b939cdcbc6264078db01e1883cfa0d9e98b
Vinciusalme <vinicius-alm@outlook.comgit config --global user.email vinicius-alm@outlook.com>
2026-07-20 19:22:34 -0300

    feat: create application startup banner

---
+++ src/main/java/com/businessflow/application/BusinessFlowApplication.java
@@ -0,0 +1,12 @@
+package com.businessflow.application;
+
+public class BusinessFlowApplication {
+    public static void main(String[] args) {
+        System.out.println("=========================================");
+        System.out.println("            BUSINESS FLOW");
+        System.out.println("Sistema de Gestão de Serviços");
+        System.out.println("=========================================");
+        System.out.println();
+        System.out.println("Inicializando Aplicação...");
+    }
+}
